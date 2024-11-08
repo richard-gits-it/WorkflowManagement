38967a9b1e54068eee7f0ca56f163c7a76711d17
Richard Lorenzo <Richard.Lorenzo@mymhc.ca>
2024-11-08 16:49:40 -0700

    Add models and migrated to database

--- WorkflowManagement/Data/ApplicationDbContext.cs
+++ WorkflowManagement/Data/ApplicationDbContext.cs
@@ -1,10 +1,14 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using WorkflowManagement.Models;
 
 namespace WorkflowManagement.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Models.Task> Tasks { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
--- WorkflowManagement/Data/Migrations/20241108234422_CreateModels.Designer.cs
+++ WorkflowManagement/Data/Migrations/20241108234422_CreateModels.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using WorkflowManagement.Data;
 
@@ -11,9 +12,10 @@ using WorkflowManagement.Data;
 namespace WorkflowManagement.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241108234422_CreateModels")]
+    partial class CreateModels
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
