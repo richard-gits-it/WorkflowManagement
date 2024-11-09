0ca837e5450c85a403314e3461b6e7c1e594673a
Richard Lorenzo <Richard.Lorenzo@mymhc.ca>
2024-11-08 17:10:36 -0700

    rearranged files

---
+++ WorkflowManagement/Data/Models/Task.cs
@@ -0,0 +1,13 @@
+﻿namespace WorkflowManagement.Data.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string? Description { get; set; }
+        public DateTime DueDate { get; set; }
+        public string Status { get; set; } // e.g., Pending, In Progress, Complete
+        public int AssignedUserId { get; set; }
+        public User AssignedUser { get; set; }
+    }
+}
